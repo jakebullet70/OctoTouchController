25d381f44f21c92515836c898601233d125e7f27
Steven De George SR <ehomecreations@gmail.com>
2023-03-31 12:01:46 +0300

    Create releaseBuild.bat

---
+++ releaseBuild.bat
@@ -0,0 +1,9 @@
+d:
+cd dev\b4x\src\OctoTouchController
+del *.apk < y
+"C:\Program Files\Anywhere Software\B4A\B4ABuilder.exe" -task=build -Configuration=Default -Optimize=true -Output=OctoTouchController
+copy Objects\*.apk 
+"C:\Program Files\Anywhere Software\B4A\B4ABuilder.exe" -task=build -Configuration=klipper -Optimize=true -Output=KlipperTouchController
+copy Objects\*.apk 
+pause
+

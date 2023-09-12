726d074c6c081bdec2f89d8373ec839cf5ed3883
Steven De George SR <ehomecreations@gmail.com>
2023-09-12 17:25:31 +0300

    Refactoring Keystore

--- releaseBuild.bat
+++ releaseBuild.bat
@@ -1,9 +1,6 @@
-d:
+c:
 cd dev\b4x\src\OctoTouchController
 del *.apk < y
 "C:\Program Files\Anywhere Software\B4A\B4ABuilder.exe" -task=build -Configuration=Default -Optimize=true -Output=OctoTouchController
 copy Objects\*.apk 
-"C:\Program Files\Anywhere Software\B4A\B4ABuilder.exe" -task=build -Configuration=klipper -Optimize=true -Output=KlipperTouchController
-copy Objects\*.apk 
 pause
-

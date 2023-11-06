731d247ca57e685258ab10fc733fbdc635f0a453
Steven De George SR <ehomecreations@gmail.com>
2023-11-06 08:46:59 +0200

    Added snapshot of java src to build batch file

--- releaseBuild.bat
+++ releaseBuild.bat
@@ -3,4 +3,6 @@ cd dev\b4x\src\OctoTouchController
 del *.apk < y
 "C:\Program Files\Anywhere Software\B4A\B4ABuilder.exe" -task=build -Configuration=Default -Optimize=true -Output=OctoTouchController
 copy Objects\*.apk 
+del OctoTouchController_java_src.7z
+7z a -t7z -r OctoTouchController_java_src.7z "Objects\src\*.*"
 pause

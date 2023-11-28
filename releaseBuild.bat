cc3da5d6e1a76edaa35a38c7c2a50d127cc5499b
Steven De George SR <ehomecreations@gmail.com>
2023-11-28 18:06:55 +0200

    Update releaseBuild.bat

--- releaseBuild.bat
+++ releaseBuild.bat
@@ -1,8 +1,21 @@
+ECHO OFF
+ECHO -------- start builds -----------------
 c:
-cd dev\b4x\src\OctoTouchController
-del *.apk < y
+cd C:\dev\b4x\src\OctoTouchController
+
+del *.apk 
+del OctoTouchController_java_src_legacy.7z 
+del OctoTouchController_java_src.7z
+
+ECHO ------------- legacy ------------------
+"C:\Program Files\Anywhere Software\B4A\B4ABuilder.exe" -task=build -Configuration=legacy -Optimize=true -Output=OctoTouchController_legacy
+copy Objects\*.apk 
+7z a -t7z -r OctoTouchController_java_src_legacy.7z "Objects\src\*.*"
+
+ECHO -------------- FOSS -------------------
 "C:\Program Files\Anywhere Software\B4A\B4ABuilder.exe" -task=build -Configuration=Default -Optimize=true -Output=OctoTouchController
 copy Objects\*.apk 
-del OctoTouchController_java_src.7z
 7z a -t7z -r OctoTouchController_java_src.7z "Objects\src\*.*"
+
+ECHO -------------- end --------------------
 pause
